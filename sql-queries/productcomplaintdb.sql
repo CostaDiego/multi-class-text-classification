d17c3b8a15f980982a388c4e970018a77f280ffb
Diego Costa <diego.szct@gmail.com>
2020-04-15 18:05:02 -0300

    SQL queries folder and create db SQL

---
+++ sql-queries/productcomplaintdb.sql
@@ -0,0 +1,15 @@
+﻿-- Database: productcomplaintdb
+
+-- DROP DATABASE productcomplaintdb;
+
+CREATE DATABASE productcomplaintdb
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+--GRANT CONNECT, TEMPORARY ON DATABASE productcomplaintdb TO public;
+GRANT ALL ON DATABASE productcomplaintdb TO postgres;
+GRANT ALL ON DATABASE productcomplaintdb TO costadiego;
+
