5a6a4d665f995b54ed02054ee001ba0244b4f157
Diego Costa <diego.szct@gmail.com>
2020-04-20 18:54:39 -0300

    DatabaseConnection class implementation

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,1 @@
-import database
+# import utils.database
--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,44 @@
 import psycopg2
+from getpass import getpass
 
-class Database(object):
-    pass
+class DatabaseConection(object):
+    """
+    """
+
+    def __init__(self, host:str, database, user):
+        try:    
+            self._con = psycopg2.connect(
+                host= host,
+                database= database,
+                user=user,
+                password = getpass(
+                    prompt= 'Input the password:\n',
+                    stream= None
+                ))
+        except:
+            print('\tFailed to establish connection!')
+
+    def send(self, sql: str):
+        try:
+            cursor = self._con.cursor()
+            cursor.execute(str(sql))
+            self._con.commit()
+            
+            return True
+
+        except:
+            return False
+
+    def request(self, sql: str):
+        try:
+            cursor = self._con.cursor()
+            cursor.execute(str(sql))
+            request = cursor.fetchall()
+
+            return request
+
+        except:
+            return None
+    
+    def closeConnection(self):
+        self._con.close()
